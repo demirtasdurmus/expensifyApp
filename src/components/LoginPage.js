d41a6b791251feb2c3a906ef50ce53e5c37d233e
Durmuş <demirtasdurmus@users.noreply.github.com>
2021-02-14 23:29:59 +0300

    styling part 1

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,14 +1,19 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
-export const LoginPage = ({startLogin}) => (
-    <div>
-        <button
-            onClick={startLogin}
-        >
-        Login
-        </button>    
+export const LoginPage = ({ startLogin }) => (
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className= "box-layout__title">Expensify</h1>
+            <p>Time to get your expenses under control!</p>
+            <button
+                className="button"
+                onClick={startLogin}
+            >
+            Login with Google
+            </button>
+        </div>
     </div>
 );
 
@@ -16,4 +21,4 @@ const MapDispatchToProps = (dispatch) => ({
     startLogin: () => dispatch(startLogin())
 });
 
-export default connect (undefined, MapDispatchToProps)(LoginPage);
+export default connect(undefined, MapDispatchToProps)(LoginPage);
