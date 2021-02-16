db8cd72ec059f7460f39a14c5fd6adaf432d6b8e
Durmuş <demirtasdurmus@users.noreply.github.com>
2021-02-16 22:48:08 +0300

    trial for env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,14 @@ import "firebase/database";
 import "firebase/auth";
 
 const firebaseConfig = {
-    // apiKey: process.env.FIREBASE_API_KEY,
-    apiKey:"AIzaSyDYAZ0pzeAX-hdjczcEt15XdwnH2Ehgt6Y",
-    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    authDomain:"durmus-expensify-app.firebaseapp.com",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    databaseURL: "https://durmus-expensify-app-default-rtdb.firebaseio.com/"
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { login, logout } from "./actions/auth";
 import getVisibleExpenses from "./selectors/expenses";
 import './styles/index.css';
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 
@@ -26,7 +27,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render( <p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage /> , document.getElementById('app'));
 
 
 
