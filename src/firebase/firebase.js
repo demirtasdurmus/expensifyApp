9beab5640d92b4a70a47a35da93e57ba163f5a43
Durmuş <demirtasdurmus@users.noreply.github.com>
2021-03-02 07:36:30 +0300

    Api keys repaired

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/database";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYAZ0pzeAX-hdjczcEt15XdwnH2Ehgt6Y",
-    authDomain: "durmus-expensify-app.firebaseapp.com",
-    databaseURL: "https://durmus-expensify-app-default-rtdb.firebaseio.com",
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
